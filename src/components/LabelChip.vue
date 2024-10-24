b1c34efe8277882da013942055d0a05f1878dec0
alyosha <one@two.three>
2024-10-24 21:26:27 +0300

    Style tag

--- src/components/LabelChip.vue
+++ src/components/LabelChip.vue
@@ -1,5 +1,5 @@
 <template>
-    <span>{{ value }}</span>
+    <span :class="`caption label-chip ${isFirst ? 'label-chip_tail' : ''}`">{{ value }}</span>
 </template>
 
 <script lang="ts">
@@ -8,7 +8,43 @@ export default {
         value: {
             required: true,
             type: String,
+        },
+        isFirst: {
+            default: false,
         }
     }
 }
 </script>
+
+<style lang="scss">
+$width-tail: 13rem;
+$border-radius: 3rem;
+
+.label-chip {
+    background: var(--color-chefs-hat);
+    
+    &:not(.label-chip_tail) {
+        padding: 4rem;
+        border-radius: $border-radius;
+    }
+    
+    &.label-chip_tail {
+        position: relative;
+        padding: 4rem 4rem 4rem 0;
+        margin: 0 0 0 $width-tail;
+        border-radius: 0 $border-radius $border-radius 0;
+    }
+
+    &_tail {
+        &::before {
+            content: url('../assets/label-tail.svg');
+            display: block;
+            width: $width-tail;
+            height: 100%;
+            position: absolute;
+            top: 0;
+            left: -$width-tail;
+        }
+    }
+}
+</style>
