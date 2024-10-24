b2b09159026480b542d922e0a5643de2fa0c23ba
alyosha <one@two.three>
2024-10-24 21:27:29 +0300

    Sass preprocessor settings

--- vite.config.ts
+++ vite.config.ts
@@ -7,5 +7,12 @@ export default defineConfig({
   server: {
     host: true,
     port: 8080
-  }
+  },
+  // css: {
+  //   preprocessorOptions: {
+  //     scss: {
+  //       additionalData: `@import "./src/styles/_global.scss";`
+  //     }
+  //   }
+  // }
 })
