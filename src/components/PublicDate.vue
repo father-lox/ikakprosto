5b960b677fd982242db53f8248b41ec7243d0509
alyosha <one@two.three>
2024-10-24 22:16:25 +0300

    Style public date

--- src/components/PublicDate.vue
+++ src/components/PublicDate.vue
@@ -1,14 +1,37 @@
 <template>
-    <span>{{ date.toLocaleDateString('ru') }}</span>
+    <span class="caption public-date">{{ choseDateFormate() }}</span>
 </template>
 
 <script lang="ts">
+import {millisecondsInDay} from '../constants'
+
 export default {
     props: {
-        date: {
+        publicDate: {
             required: true,
             type: Date
+        }
+    },
+    methods: {
+        choseDateFormate(): string {
+            const differenceBetweenCurrentAndPublicTime = Date.now() - this.publicDate.getTime()
+            const isToday = differenceBetweenCurrentAndPublicTime <= millisecondsInDay
+            const isYesterday = differenceBetweenCurrentAndPublicTime > millisecondsInDay && differenceBetweenCurrentAndPublicTime <= millisecondsInDay * 2
+
+            if (isToday) {
+                return 'Today'
+            } else if (isYesterday) {
+                return 'Yesterday'
+            } else {
+                return this.publicDate.toLocaleDateString()
+            }
         }
     }
 }
 </script>
+
+<style lang="scss">
+.public-date {
+    color: var(--color-dangerous-robot);
+}
+</style>
---
+++ src/constants.ts
@@ -0,0 +1,6 @@
+export const millisecondsInSecond = 1000
+export const secondsInMinute = 60
+export const minutesInHour = 60
+export const hoursInDay = 24
+
+export const millisecondsInDay = millisecondsInSecond * secondsInMinute * minutesInHour * hoursInDay
