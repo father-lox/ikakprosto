31bca0783253f2a5f138fd02ce96abf3606dbc3d
alyosha <one@two.three>
2024-10-25 15:57:54 +0300

    Make post

--- src/components/BlogPost.vue
+++ src/components/BlogPost.vue
@@ -1,13 +1,17 @@
 <template>
-    <article>
-        <h2>{{ props.post.title }}</h2>
-        <p>{{ props.post.body }}</p>
-        <div>
-            <like-action :count-likes="props.post.reactions.likes"></like-action>
-            <trash-action :count-trashes="props.post.reactions.dislikes"></trash-action>
-            <router-link :to="`/post/${props.post.id}`" class="action-link">Open comments</router-link>
-            <public-date :date="props.publicDate" />
-            <label-chip v-for="tag in props.post.tags" :value="tag"></label-chip>
+    <article class="post">
+        <h2 class="heading post__title">{{ props.post.title }}</h2>
+        <p class="body post__body">{{ props.post.body }}</p>
+        <div class="post__action-bar">
+            <div class="post__reactions reactions">
+                <like-action :count-likes="props.post.reactions.likes"></like-action>
+                <trash-action :count-trashes="props.post.reactions.dislikes"></trash-action>
+            </div>
+            <router-link :to="`/post/${props.post.id}`" class="post__action-link">Open comments</router-link>
+            <public-date :public-date="props.publicDate" />
+            <div class="post__tags">
+                <label-chip v-for="(tag, index) in props.post.tags" :value="tag" :is-first="index === 0"></label-chip>
+            </div>
         </div>
     </article>
 </template>
@@ -20,9 +24,41 @@ import PublicDate from './PublicDate.vue';
 import Post from '../types/post';
 
 interface PostProps {
-  post: Post,
-  publicDate: Date
+    post: Post,
+    publicDate: Date
 }
 
 const props = defineProps<PostProps>()
 </script>
+
+<style lang="scss">
+.post {
+    &__title {
+        margin: 0 0 16rem;
+    }
+
+    &__action-bar {
+        display: inline-flex;
+        align-items: center;
+        gap: 8rem;
+        margin: 24rem 0 0;
+    }
+
+    &__action-link {
+        color: var(--color-primary);
+    }
+
+    &__tags {
+        display: inline-flex;
+        gap: 4rem;
+    }
+}
+
+.reactions {
+    display: inline-flex;
+    border-radius: 50rem;
+    width: fit-content;
+    overflow: hidden;
+    gap: 1rem;
+}
+</style>
