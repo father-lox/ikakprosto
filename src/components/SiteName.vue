c92f0f05c7dc6b20e22e47fa9f30b50ffceb9e15
alyosha <one@two.three>
2024-10-25 15:57:44 +0300

    Add site name

---
+++ src/components/SiteName.vue
@@ -0,0 +1,11 @@
+<template>
+    <h1 class="site-name">ikakprosto.ru</h1>
+</template>
+
+<style lang="scss">
+.site-name {
+    color: var(--color-primary);
+    font-size: 40rem;
+    font-family: 'SFProText-Bold', sans-serif;
+}
+</style>
