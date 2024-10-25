92e26fbccf067bfb612dfe4114406c33db6b2dff
alyosha <one@two.three>
2024-10-25 18:53:59 +0300

    Change name of font-face

--- src/components/SiteName.vue
+++ src/components/SiteName.vue
@@ -1,11 +1,12 @@
 <template>
-    <h1 class="site-name">ikakprosto.ru</h1>
+    <router-link class="site-name" to="/"><h1 >ikakprosto.ru</h1></router-link>
 </template>
 
 <style lang="scss">
 .site-name {
     color: var(--color-primary);
     font-size: 40rem;
-    font-family: 'SFProText-Bold', sans-serif;
+    font-weight: bold;
+    text-decoration: none;
 }
 </style>
